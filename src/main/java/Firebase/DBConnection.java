40e8cbd19d5cec3b06bd7c8c8f0990ed6261268b
Umberto Franzese <umbertofranzes@gmail.com>
2020-12-20 14:08:24 +0100

    Update DBConnection.java

--- src/main/java/Firebase/DBConnection.java
+++ src/main/java/Firebase/DBConnection.java
@@ -10,11 +10,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
+
+/*Classe che permetterà alle altre componenti del sistema di interagire con il database
+* il costruttore della classe è privato in modo da usare metodi statici che non appartengno ad istanze della classe ma
+* alla classe stessa*/
 public class DBConnection {
     private static Firestore connection=null;
     private DBConnection(){}
 
+    /* ritornerà un oggetto di tipo Firestore in grado di comunicare con il database
+    * dichiarato come static così da appartenere alla classe e non ad un'istanza della stessa*/
     public static Firestore getConnection() throws IOException {
+        if(connection!=null) return connection;
         FileInputStream serviceAccount =
                 new FileInputStream("src/main/java/Firebase/mypersonaltrainer-firebase-admin-sdk.json");
         FirebaseOptions options = new FirebaseOptions.Builder()
@@ -25,4 +32,8 @@ public class DBConnection {
         return connection;
     }
 
+    /*stub per testare la connessione*/
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        System.out.println(DBConnection.getConnection().collection("dumb").whereEqualTo("name","dumb").get().get().getDocuments().get(0).get("name"));
+    }
 }
