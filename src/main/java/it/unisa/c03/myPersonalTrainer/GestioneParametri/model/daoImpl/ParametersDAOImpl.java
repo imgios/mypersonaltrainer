38e7d773838d884809d26680d44d165e277aa314
marco <marcosica99@gmail.com>
2020-12-23 16:55:35 +0100

    adding method in DAO to retrieve parameters by mail

--- src/main/java/it/unisa/c03/myPersonalTrainer/GestioneParametri/model/daoImpl/ParametersDAOImpl.java
+++ src/main/java/it/unisa/c03/myPersonalTrainer/GestioneParametri/model/daoImpl/ParametersDAOImpl.java
@@ -1,11 +1,17 @@
 package it.unisa.c03.myPersonalTrainer.GestioneParametri.model.daoImpl;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QuerySnapshot;
 import it.unisa.c03.myPersonalTrainer.GestioneParametri.Firebase.DBConnection;
 import it.unisa.c03.myPersonalTrainer.GestioneParametri.model.bean.Parameters;
 import it.unisa.c03.myPersonalTrainer.GestioneParametri.model.dao.ParametersDAO;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class ParametersDAOImpl implements ParametersDAO {
 
@@ -17,5 +23,44 @@ public class ParametersDAOImpl implements ParametersDAO {
             e.getMessage();
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public Parameters getByMail(String email) {
+        // Create a reference to the account collection
+        CollectionReference parameters = null;
+        try {
+            parameters = DBConnection.getConnection().collection("Parameters");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Create a query against the collection.
+        Query query = parameters.whereEqualTo("mailClient", email);
+
+        // retrieve  query results asynchronously using query.get()
+        ApiFuture<QuerySnapshot> querySnapshot = query.get();
+
+        //create Bean to return document.get("email"));
+
+        Parameters params = new Parameters();
+        try {
+            for (DocumentSnapshot document : querySnapshot.get().getDocuments()) {
+
+                Object x = document.get("fatMass");
+                System.out.println(x.getClass());
+                params.setfatMass((( Float.parseFloat(String.valueOf(document.get("fatMass"))))));
+                params.setleanMass((( Float.parseFloat(String.valueOf(document.get("leanMass"))))));
+                params.setweight((( Float.parseFloat(String.valueOf(document.get("weight"))))));
+                params.setMailClient(String.valueOf(document.get("mailClient")));
+                params.setinsertionDate(String.valueOf(document.get("insertionDate")));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return params ;
     }
 }
