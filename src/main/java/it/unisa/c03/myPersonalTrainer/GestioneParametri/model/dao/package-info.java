b1f422abdadc1e7120c02634f30e5d2409f8e600
marco <marcosica99@gmail.com>
2020-12-23 23:25:28 +0100

    deleted 2 redundant package-info

--- src/main/java/it/unisa/c03/myPersonalTrainer/GestioneParametri/model/dao/package-info.java
+++
@@ -1,2 +0,0 @@
-package it.unisa.c03.myPersonalTrainer.GestioneParametri.model.dao;
-//this packages provides the methods signature to access into the database.
--- src/main/java/it/unisa/c03/myPersonalTrainer/GestioneParametri/model/daoImpl/package-info.java
+++
@@ -1,2 +0,0 @@
-package it.unisa.c03.myPersonalTrainer.GestioneParametri.model.daoImpl;
-//this packages provides the methods implementation for dao interface
