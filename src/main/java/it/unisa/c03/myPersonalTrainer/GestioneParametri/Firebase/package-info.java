85098bedf78bd6d6d5f645dc7ffb7822883837e3
marco <marcosica99@gmail.com>
2020-12-23 12:17:50 +0100

    added package and package.info of Firebase

---
+++ src/main/java/it/unisa/c03/myPersonalTrainer/GestioneParametri/Firebase/package-info.java
@@ -0,0 +1,8 @@
+
+
+/*
+questo package permettera' a tutte le altre componenti
+del sistema un modo per connettersi al database!
+ */
+
+package it.unisa.c03.myPersonalTrainer.GestioneParametri.Firebase;
