a8b72c7268656c260701cc640505e9692fc320be
Umberto Franzese <umbertofranzes@gmail.com>
2020-12-24 01:21:21 +0100

    Update DBconnection Class and Add DB read Test

--- src/main/java/it/unisa/c03/myPersonalTrainer/firebase/DBConnection.java
+++ src/main/java/it/unisa/c03/myPersonalTrainer/firebase/DBConnection.java
@@ -1,4 +1,4 @@
-package it.unisa.c03.myPersonalTrainer.Firebase;
+package it.unisa.c03.myPersonalTrainer.firebase;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
@@ -8,28 +8,45 @@ import com.google.firebase.cloud.FirestoreClient;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 
 
 /*Classe che permetterà alle altre componenti del sistema di interagire con il database
 * il costruttore della classe è privato in modo da usare metodi statici che non appartengno ad istanze della classe ma
 * alla classe stessa*/
 public class DBConnection {
-    private static Firestore connection;
+    private static DBConnection singleton;
     private DBConnection(){}
+
+
+    public static DBConnection getInstance()
+    {
+        if (singleton==null)
+        {
+            singleton=new DBConnection();
+        }
+        return singleton;
+    }
+
 
     /* ritornerà un oggetto di tipo Firestore in grado di comunicare con il database
     * dichiarato come static così da appartenere alla classe e non ad un'istanza della stessa*/
     public static Firestore getConnection() throws IOException {
-        if(connection!=null) return connection;
+        Firestore connection=null;
         try {
-
 
             FirebaseOptions options=new FirebaseOptions.Builder().setCredentials(GoogleCredentials.getApplicationDefault()).build();
             connection=FirestoreClient.getFirestore( FirebaseApp.initializeApp(options));
             return connection;
         }
         catch (FileNotFoundException ignored){}
-        return connection;
+        return connection ;
     }
 
+    public static void main(String[] args) throws IOException {
+        HashMap<String,String> dati=new HashMap<String, String>();
+        dati.put("nome","umberto");
+        dati.put("ruolo","0");
+        DBConnection.getConnection().collection("dumb").document().set(dati);
+    }
 }
