d65ddcd19f29bf87a697e314e19433fe7aa65cbe
marco <marcosica99@gmail.com>
2020-12-28 16:50:03 +0100

    add agendaService and agendaServiceImpl

--- src/main/java/it/unisa/c03/myPersonalTrainer/agenda/service/AgendaService.java
+++ src/main/java/it/unisa/c03/myPersonalTrainer/agenda/service/AgendaService.java
@@ -1,4 +1,25 @@
 package it.unisa.c03.myPersonalTrainer.agenda.service;
 
+import it.unisa.c03.myPersonalTrainer.agenda.bean.Availability;
+
+import java.io.IOException;
+
 public interface AgendaService {
+    /**
+     * @param data the data to check
+     * @param time the time to check
+     * @return true if it's possible to insert the data
+     * @throws IllegalArgumentException
+     */
+    boolean checkAvailability(String data, String time)
+            throws IllegalArgumentException;
+
+    /**
+     * this method interact with the DAO to insert the availability into database.
+     *
+     * @param availability the availability to insert into database
+     * @throws IOException
+     */
+    void createAvailability(Availability availability)
+            throws IOException;
 }
---
+++ src/main/java/it/unisa/c03/myPersonalTrainer/agenda/service/AgendaServiceImpl.java
@@ -0,0 +1,53 @@
+package it.unisa.c03.myPersonalTrainer.agenda.service;
+
+import it.unisa.c03.myPersonalTrainer.agenda.bean.Availability;
+import it.unisa.c03.myPersonalTrainer.agenda.dao.AgendaDAO;
+import it.unisa.c03.myPersonalTrainer.agenda.dao.AgendaDAOImpl;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class AgendaServiceImpl implements AgendaService {
+
+    private static final int MIN_TIME = 9;
+    private static final int MAX_TIME = 19;
+
+    private AgendaDAO agendaDAO = new AgendaDAOImpl();
+
+    @Override
+    public boolean checkAvailability(String data, String time)
+            throws IllegalArgumentException {
+
+        boolean result = false;
+        if (!data.matches(
+                "^((19|2[0-9])[0-9]{2})-"
+                        + "(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$")) {
+            throw new IllegalArgumentException(
+                    "Formato data non valido");
+        } else {
+            LocalDate dateL = LocalDate.parse(data);
+            if (dateL.isBefore(LocalDate.now())) {
+                throw new IllegalArgumentException(
+                        "Data precedente a quella odierna");
+            }
+            if (!time.matches("([0-9]){1,2}")) {
+                throw new IllegalArgumentException(
+                        "Formato orario non valido");
+            } else if (Integer.parseInt(time) < MIN_TIME
+                    || Integer.parseInt(time) > MAX_TIME) {
+                throw new IllegalArgumentException(
+                        "Orario non valido");
+            } else {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void createAvailability(Availability availability)
+            throws IOException {
+
+        agendaDAO.insertAvailability(availability);
+    }
+}
