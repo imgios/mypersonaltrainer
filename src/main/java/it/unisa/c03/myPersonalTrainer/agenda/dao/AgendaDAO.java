a5592b2cff30d9e9cb5d7fb7937b082db683d4d1
marco <marcosica99@gmail.com>
2020-12-29 13:16:07 +0100

    updated, including (removeAvail, findByDate, findByDateAndTime)

--- src/main/java/it/unisa/c03/myPersonalTrainer/agenda/dao/AgendaDAO.java
+++ src/main/java/it/unisa/c03/myPersonalTrainer/agenda/dao/AgendaDAO.java
@@ -1,21 +1,61 @@
 package it.unisa.c03.myPersonalTrainer.agenda.dao;
-
-import it.unisa.c03.myPersonalTrainer.agenda.bean.Appointment;
 import it.unisa.c03.myPersonalTrainer.agenda.bean.Availability;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * this class provides the methods to make the CRUD operations into database.
+ */
 public interface AgendaDAO {
 
 
     /**
-     * interact with database to insert the availability .
-     *
+     * interact with database to insert the availability.
+     * @return true if the insertion is done, false otherwise.
      * @param availability the availability to insert into database
      * @throws IOException
      */
     boolean insertAvailability(Availability availability) throws IOException;
+
+    /**
+     * return the availability list from the date in input.
+     *
+     * @param date for find availability
+     * @return availability list for this date
+     * @throws ExecutionException
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    List<Availability> findAvailabilityByDate(
+            String date)
+            throws ExecutionException, IOException, InterruptedException;
+
+    /**
+     * enable to remove the availability.
+     *
+     * @param availability the availability to remove
+     * @return true if the remove is done.
+     * @throws IOException
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    boolean deleteAvailability(
+            Availability availability)
+            throws IOException, ExecutionException, InterruptedException;
+
+    /**
+     * retrieve the availability with this  time and date.
+     *
+     * @param date
+     * @param time
+     * @return the availability that contains this time and date.
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    Availability findAvailabilityByDateAndTime(
+            String date, int time)
+            throws IOException, InterruptedException, ExecutionException;
 }
--- src/main/java/it/unisa/c03/myPersonalTrainer/agenda/dao/AgendaDAOImpl.java
+++ src/main/java/it/unisa/c03/myPersonalTrainer/agenda/dao/AgendaDAOImpl.java
@@ -1,24 +1,91 @@
 package it.unisa.c03.myPersonalTrainer.agenda.dao;
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import it.unisa.c03.myPersonalTrainer.agenda.bean.Availability;
-import it.unisa.c03.myPersonalTrainer.agenda.bean.Appointment;
 import it.unisa.c03.myPersonalTrainer.firebase.DBConnection;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public final class AgendaDAOImpl implements AgendaDAO {
+/**
+ * this class provides the methods to make the
+ * CRUD operations with the availability.
+ */
+public final class AgendaDAOImpl
+        implements AgendaDAO {
 
-
+    /**
+     * @param availability the availability to insert into database
+     * @return true if the insertion occurred, false otherwise
+     * @throws IOException
+     */
     @Override
-    public boolean insertAvailability(Availability availability)
+    public boolean insertAvailability(
+            Availability availability)
             throws IOException {
         DBConnection.getConnection().collection(
                 "Availability").add(availability);
         return true;
+    }
+
+    @Override
+    public List<Availability> findAvailabilityByDate(
+            String date)
+            throws IOException, InterruptedException, ExecutionException {
+        Firestore connection = DBConnection.getConnection();
+        List<Availability> dispo = null;
+        Query query = connection.collection("Availability")
+                .whereEqualTo("date", date);
+        ApiFuture<QuerySnapshot> querySnapShot =
+                query.get();
+        List<QueryDocumentSnapshot> docs =
+                querySnapShot.get().getDocuments();
+        Stream<Availability> sa =
+                docs.stream().map(queryDocumentSnapshot ->
+                queryDocumentSnapshot.toObject(Availability.class));
+        dispo = sa.collect(Collectors.toList());
+        return dispo;
+    }
+
+
+    public boolean deleteAvailability(
+            Availability availability)
+            throws IOException, ExecutionException, InterruptedException {
+
+        Firestore connection =
+                DBConnection.getConnection();
+        List<QueryDocumentSnapshot> lqds =
+                connection.collection("Availability").
+                whereEqualTo("date", availability.getDate())
+                .whereEqualTo("time", availability.getTime()).
+                        get().get().getDocuments();
+        for (QueryDocumentSnapshot document : lqds) {
+            document.getReference().delete();
+        }
+        return true;
+    }
+
+
+    @Override
+    public Availability findAvailabilityByDateAndTime(
+            String date, int time)
+            throws IOException, InterruptedException, ExecutionException {
+        Firestore connection = DBConnection.getConnection();
+        List<Availability> dispo = null;
+        Query query = connection.collection("Availability")
+                .whereEqualTo("date", date).whereEqualTo("time", time);
+        ApiFuture<QuerySnapshot> querySnapShot = query.get();
+        List<QueryDocumentSnapshot> docs = querySnapShot.get().getDocuments();
+        Stream<Availability> sa = docs.stream().map(queryDocumentSnapshot ->
+                queryDocumentSnapshot.toObject(Availability.class));
+        dispo = sa.collect(Collectors.toList());
+        return dispo.get(0);
     }
 }
