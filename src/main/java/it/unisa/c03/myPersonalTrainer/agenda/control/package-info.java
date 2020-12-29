85d0adbc9b66c0109fca301a83668de57916f69f
marco <marcosica99@gmail.com>
2020-12-29 13:14:42 +0100

    adding package-info

--- src/main/java/it/unisa/c03/myPersonalTrainer/agenda/control/package-info.java
+++ src/main/java/it/unisa/c03/myPersonalTrainer/agenda/control/package-info.java
@@ -1,4 +1,5 @@
 /**
- * this package provides the controller to manage the appointment and the availability.
+ * this package provides the controller to
+ * manage the appointment and the availability.
  */
 package it.unisa.c03.myPersonalTrainer.agenda.control;
