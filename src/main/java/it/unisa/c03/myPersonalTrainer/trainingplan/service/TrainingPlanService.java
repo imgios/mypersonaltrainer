a32ff1a3575269db44194aa88afc44cc05f2b9c3
marco <marcosica99@gmail.com>
2020-12-30 18:06:57 +0100

    service + service impl

--- src/main/java/it/unisa/c03/myPersonalTrainer/trainingplan/service/TrainingPlanService.java
+++ src/main/java/it/unisa/c03/myPersonalTrainer/trainingplan/service/TrainingPlanService.java
@@ -2,11 +2,14 @@ package it.unisa.c03.myPersonalTrainer.trainingplan.service;
 
 import it.unisa.c03.myPersonalTrainer.trainingplan.bean.TrainingPlan;
 
+import java.io.IOException;
+
 public interface TrainingPlanService {
 
 
-    public boolean checkExercise(String exercise, String repetitions, String series, String recoveryTime)  throws IllegalArgumentException;
+     boolean checkExercise(String exercise, String repetitions, String series, String recoveryTime)  throws IllegalArgumentException;
 
+     boolean createTrainingPlan(TrainingPlan trainingPlan) throws IOException;
 
 
 }
--- src/main/java/it/unisa/c03/myPersonalTrainer/trainingplan/service/TrainingPlanServiceImpl.java
+++ src/main/java/it/unisa/c03/myPersonalTrainer/trainingplan/service/TrainingPlanServiceImpl.java
@@ -1,39 +1,48 @@
 package it.unisa.c03.myPersonalTrainer.trainingplan.service;
 
+import it.unisa.c03.myPersonalTrainer.trainingplan.bean.TrainingPlan;
+import it.unisa.c03.myPersonalTrainer.trainingplan.dao.TrainingPlanDAO;
+
+import java.io.IOException;
+
 public class TrainingPlanServiceImpl implements TrainingPlanService {
 
+    private TrainingPlanDAO trainingPlanDAO;
 
-    /*
-        @Override
-        public TrainingPlan createTrainingPlan() {
+    public TrainingPlanServiceImpl(TrainingPlanDAO trainingPlanDAO) {
+        this.trainingPlanDAO = trainingPlanDAO;
+    }
 
-        }
-    */
     @Override
     public boolean checkExercise(String exercise, String repetitions, String series, String recoveryTime) throws IllegalArgumentException {
 
         boolean result = false;
 
-        if (!(exercise.matches(" /^[a-zA-z]+$/"))) {
+        if (!exercise.matches("^[a-zA-Z]+$")) {
             throw new IllegalArgumentException("invalid exercise");
         } else if (exercise.length() < 4 || exercise.length() > 25) {
             throw new IllegalArgumentException("invalid exercise length");
-        } else if (!(series.matches(" /^[0-9]+$/"))) {
-            throw new IllegalArgumentException("invalid exercise format");
-        } else if (Integer.parseInt(series) < 1 || Integer.parseInt(series) >= 20) {
+        } else if (!series.matches("^[0-9]+$")) {
+            throw new IllegalArgumentException("invalid series format");
+        } else if (series.length() < 1 || series.length() >= 3) {
             throw new IllegalArgumentException("invalid series length");
-        } else if (!(repetitions.matches(" /^[0-9]+$/"))) {
+        } else if (!repetitions.matches("^[0-9]+$")) {
             throw new IllegalArgumentException("invalid repetitions format");
-        } else if (Integer.parseInt(repetitions) < 1 || Integer.parseInt(repetitions) >= 50) {
+        } else if (repetitions.length() < 1 || repetitions.length() >= 3) {
             throw new IllegalArgumentException("invalid repetitions length");
-        } else if (!(recoveryTime.matches(" /^[0-9]+$/"))) {
+        } else if (!recoveryTime.matches("^[0-9]+$")) {
             throw new IllegalArgumentException("invalid recoveryTime format");
-        } else if (Integer.parseInt(recoveryTime) < 1 || Integer.parseInt(recoveryTime) >= 300) {
+        } else if (recoveryTime.length() < 1 || recoveryTime.length() >= 3) {
             throw new IllegalArgumentException("invalid recoveryTime length");
         } else {
             result = true;
         }
 
         return result;
+    }
+
+    @Override
+    public boolean createTrainingPlan(TrainingPlan trainingPlan) throws IOException {
+        return trainingPlanDAO.insertTrainingPlan(trainingPlan);
     }
 }
