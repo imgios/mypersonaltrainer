4ccf1b78cb653d218d6642156217e3fc5fd9f68d
marco <marcosica99@gmail.com>
2020-12-31 17:32:49 +0100

    added: package-info for agenda.

---
+++ src/main/java/it/unisa/c03/myPersonalTrainer/agenda/bean/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * this provides the beans: Appointment, Availability.
+ */
+package it.unisa.c03.myPersonalTrainer.agenda.bean;
---
+++ src/main/java/it/unisa/c03/myPersonalTrainer/agenda/dao/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * this package provides the classes that make the CRUD operation.
+ */
+package it.unisa.c03.myPersonalTrainer.agenda.dao;
