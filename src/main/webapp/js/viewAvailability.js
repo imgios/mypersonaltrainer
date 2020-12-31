2d501bf93aed6116597d7fa467f141f951cdbfe1
marco <marcosica99@gmail.com>
2020-12-31 10:36:03 +0100

    add js and jsp for viewAvailability
    (with ajax,json)

---
+++ src/main/webapp/js/viewAvailability.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $('#divError').empty();
+    $('#divSuccess').empty();
+    $('#divError').hide();
+    $('#divSuccess').hide();
+});
+
+$(document).ready(function () {
+    $("#sendDate").click(function () {
+        $('#divError').hide();
+        $('#divSuccess').hide();
+        var dataSelected = $("#iddataSelected").val();
+
+
+        $.get('view-availability', {
+            "dataSelected": dataSelected,
+        }, function (data) {
+            if (data == 0) {
+                $('#divError').empty().append("<p>Non ci sono disponibilita' per il giorno " + dataSelected + "</p>");
+                $('#divError').show();
+
+            } else {
+
+                const obj = JSON.parse(data);
+
+                var str = "";
+                str += "<table class=" + "table table-hover table-dark" + "> <thead> <tr> <th scope=" + "col" + ">Data</th> <th scope=" + "col" + ">Orario</th>  </tr> </thead> <tbody>";
+
+                $.each(obj, function (i, name) {
+                    str += "<tr> <td scope=" + "row" + ">"
+                        + name.date
+                        + "</td>"
+                        + "<td scope=" + "row" + ">"
+                        + name.time
+                        + " </td> </tr>";
+
+                });
+                str += "</tbody> </table>"
+
+
+                $('#divSuccess').empty().append("<p>" + str + "</p>");
+                $('#divSuccess').show();
+            }
+        });
+        $('#divError').empty();
+        $('#divSuccess').empty();
+        $('#divError').hide();
+        $('#divSuccess').hide();
+    });
+});
