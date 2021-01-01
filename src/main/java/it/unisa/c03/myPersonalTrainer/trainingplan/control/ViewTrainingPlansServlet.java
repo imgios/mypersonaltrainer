98265cc30ea0a0f5097308d2e7df8330f8dbfd91
Emma <emma.manzo@yahoo.it>
2021-01-01 22:06:04 +0100

    Add jsp and servlet

---
+++ src/main/java/it/unisa/c03/myPersonalTrainer/trainingplan/control/ViewTrainingPlansServlet.java
@@ -0,0 +1,22 @@
+package it.unisa.c03.myPersonalTrainer.trainingplan.control;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ViewTrainingPlansServlet", value = "ViewTrainingPlansServlet")
+
+public class ViewTrainingPlansServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+}
---
+++ src/main/webapp/ViewTrainingPlan.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: em
+  Date: 31/12/2020
+  Time: 17:08
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Le Mie Schede</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<p>provaaaaaa</p>
+
+</body>
+</html>
