8f96851b85d6129407acc34a65b89afe3f07b426
Giampiero <frr.giampiero@gmail.com>
2021-01-03 18:28:18 +0100

    Add fix testing

--- src/main/java/it/unisa/c03/myPersonalTrainer/agenda/package-info.java
+++ src/main/java/it/unisa/c03/myPersonalTrainer/agenda/package-info.java
@@ -1,1 +1,5 @@
+/**
+ *
+ *  This package contains the implementation of Agenda.
+ */
 package it.unisa.c03.myPersonalTrainer.agenda;
--- src/test/java/it/unisa/c03/myPersonalTrainer/account/dao/AccountDAOImplTest.java
+++ src/test/java/it/unisa/c03/myPersonalTrainer/account/dao/AccountDAOImplTest.java
@@ -25,14 +25,14 @@ class AccountDAOImplTest {
     @BeforeAll
     static void setUp() throws IOException {
 
-        accountTest = new Account("clientName", "clientSurname","332","hismail@italy.com","hispassword1",0);
+        accountTest = new Account("Account Test", "Account Test","3401212123","cerca@utente.it","PasswordTest1",0);
         DBConnection.getConnection().collection("Account").add(accountTest);
     }
 
 
     @AfterAll
     static void clean() throws IOException, ExecutionException, InterruptedException {
-        List<QueryDocumentSnapshot> lqds = DBConnection.getConnection().collection("Account").whereEqualTo("email","hismail@italy.com").get().get().getDocuments();
+        List<QueryDocumentSnapshot> lqds = DBConnection.getConnection().collection("Account").whereEqualTo("email","cerca@utente.it").get().get().getDocuments();
 
         for(QueryDocumentSnapshot document : lqds)
         {
@@ -44,7 +44,7 @@ class AccountDAOImplTest {
     void findAccountByEmail() throws InterruptedException, ExecutionException, IOException {
 
         AccountDAO dao = new AccountDAOImpl();
-        Account accountToSearch = dao.findAccountByEmail("hismail@italy.com");
+        Account accountToSearch = dao.findAccountByEmail("cerca@utente.it");
 
         assertEquals(accountTest.getEmail(), accountToSearch.getEmail());
     }
@@ -55,9 +55,30 @@ class AccountDAOImplTest {
         assertEquals(true, dao.updatePassword(accountTest.getEmail(),"changedPassword56"));
     }
 
+
+    /* da inserire per vedere se effettivamente ce l'account o meno
+    @BeforeAll
+    static void searchifexistaccount() throws IOException {
+
+        accountTest = new Account("Account Test", "Account Test","3401212123","cerca@utente.it","PasswordTest1",0);
+        DBConnection.getConnection().collection("Account").add(accountTest);
+    }
+    */
+
+
+    @AfterAll
+    static void afterinsertaccount() throws IOException, ExecutionException, InterruptedException {
+        List<QueryDocumentSnapshot> lqds = DBConnection.getConnection().collection("Account").whereEqualTo("email","admin@admin.it").get().get().getDocuments();
+
+        for(QueryDocumentSnapshot document : lqds)
+        {
+            document.getReference().delete();
+        }
+    }
+
     @Test
     void saveAccount() throws IOException {
-        Account utente = new Account("Admin", "Admin", "0000000000", "admin@adminnnnn.it", "Adminaccount00", 1);
+        Account utente = new Account("Admin", "Admin", "0000000000", "admin@admin.it", "Adminaccount00", 1);
 
         AccountDAO adao = new AccountDAOImpl();
         assertTrue(adao.saveAccount(utente));
