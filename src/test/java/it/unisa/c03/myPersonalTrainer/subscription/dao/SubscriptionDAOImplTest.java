55de4af6fc1ef3174067584694c341eb02f745d9
mic-ian <michele.iannucci9@gmail.com>
2021-01-04 22:31:57 +0100

    Add testing for Subscription Dao

---
+++ src/test/java/it/unisa/c03/myPersonalTrainer/subscription/dao/SubscriptionDAOImplTest.java
@@ -0,0 +1,64 @@
+package it.unisa.c03.myPersonalTrainer.subscription.dao;
+
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import it.unisa.c03.myPersonalTrainer.account.bean.Account;
+import it.unisa.c03.myPersonalTrainer.firebase.DBConnection;
+import it.unisa.c03.myPersonalTrainer.subscription.bean.Subscription;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubscriptionDAOImplTest {
+/*
+    Subscription subTest;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        subTest = new Subscription("subs@looking.com","2021-03-01","30");
+        DBConnection.getConnection().collection("Subscription").add(subTest);
+    }
+
+
+    @AfterEach
+    void clean() throws IOException, ExecutionException, InterruptedException {
+        List<QueryDocumentSnapshot> lqds = DBConnection.getConnection().collection("Subscription").whereEqualTo("customerMail","subs@looking.com").get().get().getDocuments();
+
+        for(QueryDocumentSnapshot document : lqds)
+        {
+            document.getReference().delete();
+        }
+    }
+
+    @AfterAll
+    static void clean2() throws IOException, ExecutionException, InterruptedException {
+        List<QueryDocumentSnapshot> lqds = DBConnection.getConnection().collection("Subscription").whereEqualTo("customerMail","subs@looking.com").get().get().getDocuments();
+
+        for(QueryDocumentSnapshot document : lqds)
+        {
+            document.getReference().delete();
+        }
+    }
+
+     */
+
+    @Test
+    void getSubscriptionbyEmail() throws InterruptedException, ExecutionException, IOException {
+
+        SubscriptionDAO subDao = new SubscriptionDAOImpl();
+        Subscription subToSearch = subDao.getSubscriptionbyEmail("subs@looking.com");
+        assertEquals("subs@looking.com", subToSearch.getCustomerMail());
+    }
+
+    @Test
+    void getAllSubscriptions() throws InterruptedException, ExecutionException, IOException {
+        SubscriptionDAO subDao = new SubscriptionDAOImpl();
+        ArrayList<Subscription> voidList = new ArrayList<>();
+        ArrayList<Subscription> listToReturn = subDao.getAllSubscriptions();
+        assertNotEquals(voidList.size(),listToReturn.size());
+    }
+}
