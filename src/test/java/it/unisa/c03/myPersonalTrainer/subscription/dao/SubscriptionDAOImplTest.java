ae608e2827f3bbf1d6c36fb2d8475f879e26f271
marco <marcosica99@gmail.com>
2021-01-07 21:39:37 +0100

    add SubscriptionDaoImplTest

--- src/test/java/it/unisa/c03/myPersonalTrainer/subscription/dao/SubscriptionDAOImplTest.java
+++ src/test/java/it/unisa/c03/myPersonalTrainer/subscription/dao/SubscriptionDAOImplTest.java
@@ -2,6 +2,8 @@ package it.unisa.c03.myPersonalTrainer.subscription.dao;
 
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import it.unisa.c03.myPersonalTrainer.account.bean.Account;
+import it.unisa.c03.myPersonalTrainer.account.dao.AccountDAO;
+import it.unisa.c03.myPersonalTrainer.account.dao.AccountDAOImpl;
 import it.unisa.c03.myPersonalTrainer.firebase.DBConnection;
 import it.unisa.c03.myPersonalTrainer.subscription.bean.Subscription;
 import org.junit.jupiter.api.*;
@@ -59,6 +61,13 @@ class SubscriptionDAOImplTest {
         SubscriptionDAO subDao = new SubscriptionDAOImpl();
         ArrayList<Subscription> voidList = new ArrayList<>();
         ArrayList<Subscription> listToReturn = subDao.getAllSubscriptions();
-        assertNotEquals(voidList.size(),listToReturn.size());
+        assertNotEquals(voidList.size(), listToReturn.size());
+    }
+
+
+    @Test
+    void updatePassword() throws IOException, ExecutionException, InterruptedException {
+        SubscriptionDAO subDao = new SubscriptionDAOImpl();
+        assertEquals(true, subDao.updateSentNotification("marcosica99@libero.it"));
     }
 }
