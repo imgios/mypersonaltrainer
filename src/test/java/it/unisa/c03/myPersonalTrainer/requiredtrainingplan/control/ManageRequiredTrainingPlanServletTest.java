c8d28a6b42a225630ec194f5aa520bcdf5d2cc78
Edoardo Iannaccone <edonov99@gmail.com>
2021-01-09 04:54:41 +0100

    Add testing

---
+++ src/test/java/it/unisa/c03/myPersonalTrainer/requiredtrainingplan/control/ManageRequiredTrainingPlanServletTest.java
@@ -0,0 +1,121 @@
+package it.unisa.c03.myPersonalTrainer.requiredtrainingplan.control;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+
+
+import it.unisa.c03.myPersonalTrainer.account.bean.Account;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import it.unisa.c03.myPersonalTrainer.requiredtrainingplan.bean.RequiredTrainingPlan;
+import it.unisa.c03.myPersonalTrainer.requiredtrainingplan.control.ManageRequiredTrainingPlanServlet;
+import it.unisa.c03.myPersonalTrainer.requiredtrainingplan.dao.RequiredTrainingPlanDAO;
+import it.unisa.c03.myPersonalTrainer.requiredtrainingplan.dao.RequiredTrainingPlanDAOImpl;
+import it.unisa.c03.myPersonalTrainer.requiredtrainingplan.service.RequiredTrainingPlanService;
+import it.unisa.c03.myPersonalTrainer.requiredtrainingplan.service.RequiredTrainingPlanServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+    class ManageRequiredTrainingPlanServletTest {
+
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+        RequiredTrainingPlanServiceImpl requireService = Mockito.mock(RequiredTrainingPlanServiceImpl.class);
+        RequiredTrainingPlanDAO requiredTrainingDAO= Mockito.mock(RequiredTrainingPlanDAOImpl.class);
+        RequiredTrainingPlan requireB = Mockito.mock(RequiredTrainingPlan.class);
+        RequiredTrainingPlanServiceImpl requiredTrainingPlanService = Mockito.mock(RequiredTrainingPlanServiceImpl.class);
+
+        boolean checked = false;
+
+        String email = "test@test.it";
+        int required = 1;
+        @Test
+        void doPostPass() throws IOException, ServletException, ExecutionException, InterruptedException {
+            Mockito.when(request.getParameter("email")).thenReturn(email);
+            assertEquals(email, request.getParameter("email"));
+            Mockito.when(request.getParameter("required")).thenReturn(String.valueOf(required));
+            assertEquals( "1", request.getParameter("required"));
+           // RequiredTrainingPlan requireB = new RequiredTrainingPlan (request.getParameter("email"), request.getIntHeader("required"));
+            requireB= new RequiredTrainingPlan(request.getParameter("email"), request.getIntHeader("required"));
+            requiredTrainingPlanService = new RequiredTrainingPlanServiceImpl(requiredTrainingDAO);
+
+            HttpSession session = Mockito.mock(HttpSession.class);
+            Mockito.when(request.getSession()).thenReturn(session);
+
+            doNothing().when(session).removeAttribute(anyString());
+            doNothing().when(session).setAttribute(anyString(),any());
+            doNothing().when(response).sendRedirect(anyString());
+
+            Mockito.when(requireService.registerRequest(Mockito.any())).thenReturn(true);
+
+            assertTrue(requireService.registerRequest(requireB));
+            new ManageRequiredTrainingPlanServlet().doPost(request,response);
+        }
+
+        @Test
+        void doPostFalse() throws IOException, ServletException, ExecutionException, InterruptedException {
+
+            Mockito.when(request.getParameter("username")).thenReturn("TestUsername");
+            assertEquals("TestUsername", request.getParameter("username"));
+            Mockito.when(request.getParameter("surname")).thenReturn("TestSurname");
+            Mockito.when(request.getParameter("phone")).thenReturn("0000000000");
+            Mockito.when(request.getParameter("email")).thenReturn("test@test.pt");
+            Mockito.when(request.getParameter("password")).thenReturn("Test001");
+            Mockito.when(request.getParameter("role")).thenReturn("0");
+            Account user = new Account(request.getParameter("username"), request.getParameter("surname"),
+                    request.getParameter("phone"), request.getParameter("email"),
+                    request.getParameter("passowrd"), request.getIntHeader("role"));
+
+            Mockito.when(requireService.registerRequest(Mockito.any())).thenReturn(false);
+
+            HttpSession session = Mockito.mock(HttpSession.class);
+            Mockito.when(request.getSession()).thenReturn(session);
+
+            doNothing().when(session).removeAttribute(anyString());
+            doNothing().when(session).setAttribute(anyString(), any());
+            doNothing().when(response).sendRedirect(anyString());
+
+            new ManageRequiredTrainingPlanServlet().doPost(request,response);
+        }
+
+        @Test
+        void doGet() throws IOException, ServletException, ExecutionException, InterruptedException {
+
+            Mockito.when(request.getParameter("username")).thenReturn("TestUsername");
+            //assertEquals("TestUsername", request.getParameter("username"));
+            Mockito.when(request.getParameter("surname")).thenReturn("TestSurname");
+            Mockito.when(request.getParameter("phone")).thenReturn("0000000000");
+            Mockito.when(request.getParameter("email")).thenReturn("test@test.it");
+            Mockito.when(request.getParameter("password")).thenReturn("Test001");
+            Mockito.when(request.getParameter("role")).thenReturn("0");
+
+            Account user = new Account (request.getParameter("username"),
+                    request.getParameter("surname"), request.getParameter("phone"), request.getParameter("email"), request.getParameter("passowrd"), request.getIntHeader("role"));
+
+            Mockito.when(requireService.registerRequest(any())).thenReturn(true);
+
+            HttpSession session = Mockito.mock(HttpSession.class);
+            Mockito.when(request.getSession()).thenReturn(session);
+
+            doNothing().when(session).removeAttribute(anyString());
+            doNothing().when(session).setAttribute(anyString(), any());
+            doNothing().when(response).sendRedirect(anyString());
+
+
+            //new CreateAccountServlet().doPost(request,response);
+            new ManageRequiredTrainingPlanServlet().doGet(request,response);
+        }
+
+
+    }
---
+++ src/test/java/it/unisa/c03/myPersonalTrainer/requiredtrainingplan/service/RequiredTrainingPlanServiceImplTest.java
@@ -0,0 +1,143 @@
+package it.unisa.c03.myPersonalTrainer.requiredtrainingplan.service;
+
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import it.unisa.c03.myPersonalTrainer.firebase.DBConnection;
+import it.unisa.c03.myPersonalTrainer.requiredtrainingplan.bean.RequiredTrainingPlan;
+import it.unisa.c03.myPersonalTrainer.requiredtrainingplan.dao.RequiredTrainingPlanDAO;
+import it.unisa.c03.myPersonalTrainer.requiredtrainingplan.service.RequiredTrainingPlanService;
+import it.unisa.c03.myPersonalTrainer.requiredtrainingplan.service.RequiredTrainingPlanServiceImpl;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
+
+class RequiredTrainingPlanServiceImplTest {
+
+    @Test
+    public void searchAccountByEmailFalse() throws InterruptedException, ExecutionException, IOException {
+        RequiredTrainingPlanDAO requiredTrainingPlanDAO = Mockito.mock(RequiredTrainingPlanDAO.class) ;
+        RequiredTrainingPlan a = new RequiredTrainingPlan();
+        a.setEmail(null);
+        Mockito.when(requiredTrainingPlanDAO.findAccountByEmail(anyString())).thenReturn(a);
+
+        RequiredTrainingPlanService service  = new RequiredTrainingPlanServiceImpl(requiredTrainingPlanDAO);
+        assertEquals(false, service.searchAccountByEmail("mailnot@italy.it"));
+
+    }
+
+    @Test
+    public void searchAccountByEmailTrue() throws InterruptedException, ExecutionException, IOException {
+        RequiredTrainingPlanDAO requiredTrainingPlanDAO = Mockito.mock(RequiredTrainingPlanDAO.class) ;
+        RequiredTrainingPlan a = new RequiredTrainingPlan();
+        a.setEmail("mail@mail.com");
+        Mockito.when(requiredTrainingPlanDAO.findAccountByEmail(anyString())).thenReturn(a);
+        RequiredTrainingPlanService service  = new RequiredTrainingPlanServiceImpl(requiredTrainingPlanDAO);
+        assertEquals(true, service.searchAccountByEmail("cliente@gmail.com"));
+    }
+
+
+    @Test
+    public void changeRequestFalse() throws IOException, ExecutionException, InterruptedException {
+        RequiredTrainingPlanDAO requiredTrainingPlanDAO = Mockito.mock(RequiredTrainingPlanDAO.class) ;
+        when(requiredTrainingPlanDAO.updateRequest(anyString(), anyInt())).thenReturn(false);
+        RequiredTrainingPlanService service  = new RequiredTrainingPlanServiceImpl(requiredTrainingPlanDAO);
+        assertEquals(false, service.changeRequest("cliente@gmail.com", 0));
+    }
+
+    @Test
+    public void changePasswordTrue() throws IOException, ExecutionException, InterruptedException {
+        RequiredTrainingPlanDAO requiredTrainingPlanDAO = Mockito.mock(RequiredTrainingPlanDAO.class) ;
+        when(requiredTrainingPlanDAO.updateRequest(anyString(),anyInt())).thenReturn(true);
+        RequiredTrainingPlanService service  = new RequiredTrainingPlanServiceImpl(requiredTrainingPlanDAO);
+        assertEquals(true, service.changeRequest("cliente@gmail.com", 0));
+    }
+
+    @BeforeAll
+    static void deleteAccountifExist() throws IOException, ExecutionException, InterruptedException {
+
+        List<QueryDocumentSnapshot> list_account = DBConnection.getConnection()
+                .collection("RequiredTrainingPlan").whereEqualTo("email", "trainerino@testing.com").get().get().getDocuments();
+        for (QueryDocumentSnapshot document : list_account) {
+            document.getReference().delete();
+        }
+    }
+
+    @AfterAll
+    static void deleteAccountAfterInsert() throws IOException, ExecutionException, InterruptedException {
+        List<QueryDocumentSnapshot> list_account = DBConnection.getConnection().collection("RequiredTrainingPlan").whereEqualTo("email","trainerino@testing.com").get().get().getDocuments();
+
+        for(QueryDocumentSnapshot document : list_account)
+        {
+            document.getReference().delete();
+        }
+    }
+
+    @Test
+    void registerAccountTrue() throws IOException, IllegalArgumentException,
+            ExecutionException, InterruptedException {
+
+        RequiredTrainingPlan require = new RequiredTrainingPlan("test@test.it", 0);
+
+        RequiredTrainingPlanDAO requiredDAO = Mockito.mock(RequiredTrainingPlanDAO.class);
+
+        RequiredTrainingPlanService requires = new RequiredTrainingPlanServiceImpl(requiredDAO);
+
+        Mockito.when(requiredDAO.findAccountByEmail(anyString())).thenReturn(require);
+        Mockito.when(requiredDAO.storeRequest(any())).thenReturn(true);
+        RequiredTrainingPlan user_test = new RequiredTrainingPlan("test_1@test.it", 0);
+
+        assertTrue(requires.registerRequest(user_test));
+
+    }
+
+
+    @BeforeAll
+    static void insertbeforetest() throws IOException {
+
+        RequiredTrainingPlan user = new RequiredTrainingPlan("trainerino@testing.com", 0);
+        DBConnection.getConnection().collection("RequiredTrainingPlan").add(user);
+    }
+
+    @AfterAll
+    static void deleteaftertest() throws IOException, ExecutionException, InterruptedException {
+        List<QueryDocumentSnapshot> list_account = DBConnection.getConnection().collection("RequiredTrainingPlan").whereEqualTo("email","trainerino@testing.com").get().get().getDocuments();
+
+        for(QueryDocumentSnapshot document : list_account)
+        {
+            document.getReference().delete();
+        }
+    }
+
+   /* @Test
+    void registerAccountFalse() throws IOException, IllegalArgumentException,
+            ExecutionException, InterruptedException {
+
+        RequiredTrainingPlan user = new RequiredTrainingPlan("trainerino@testing.com", 1);
+
+        RequiredTrainingPlanDAO accountDAO = Mockito.mock(RequiredTrainingPlanDAO.class);
+        RequiredTrainingPlanService accountS = new RequiredTrainingPlanServiceImpl(accountDAO);
+
+        Mockito.when(accountDAO.findAccountByEmail(anyString())).thenReturn(user);
+
+        RequiredTrainingPlan user_test = new RequiredTrainingPlan("trainerino@testing.com", 1);
+
+        // assertFalse(accountS.registerAccount(user_test));
+        assertThrows(IllegalArgumentException.class, () -> {
+                    accountS.registerRequest(user_test);
+                }
+        );
+    }*/
+
+}
